97051ccf633ea84da6de455754ad56ebd15e88d8
SandeepaDilakshana <138925290+SandeepaDilakshana@users.noreply.github.com>
2024-12-03 16:05:39 +0000

    finalizing Product.js

--- flowerui/src/components/Product.js
+++ flowerui/src/components/Product.js
@@ -1,25 +1,29 @@
 import '../assets/CSS/layout.css';
-import { flowers } from './FlowerDB';
-
+import { useState } from 'react';
 export default function Product() {
+
+    const [quantity, setQuantity] = useState(0);
+
+    const handleCart = () => {
+        if (quantity > 0) {
+            addToCart(flower, quantity);
+            setQuantity(0);
+        }
+    }
 
     return (
         <div className="grid-item">
-            {
-                flowers.map((flower) =>
-                    <div class="card">
-                        <img src={require('../assets/image/' + flower.img)}/>
-                        <div class="card-body">
-                            <h5 class="card-title">{flower.name} Price:{flower.price}</h5>
-                            <div class="quantity-container">
-                                <label for="quantity">Quantity:</label>
-                                <input type="number" id="quantity" name="quantity" />
-                            </div>
-                            <button class="card-button">Add to Cart</button>
-                        </div>
+            <div class="card">
+                <img src={require('../assets/image/' + flower.img)} alt={flower.name}/>
+                <div class="card-body">
+                    <h5 class="card-title">{flower.name} Price:{flower.price}</h5>
+                    <div class="quantity-container">
+                        <label for="quantity">Quantity:</label>
+                        <input type="number" id="quantity" name="quantity" />
                     </div>
-                )
-            }              
+                    <button class="card-button" onClick={handleCart}>Add to Cart</button>
+                </div>
+            </div>
         </div>
     );
 }
