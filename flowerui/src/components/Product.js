f9d7a47fa4120e24241becf3f0b51d446f29fc18
SandeepaDilakshana <138925290+SandeepaDilakshana@users.noreply.github.com>
2024-12-03 15:20:49 +0000

    update Product.js by passing pictures,prices and names of flowers

--- flowerui/src/components/Product.js
+++ flowerui/src/components/Product.js
@@ -1,21 +1,25 @@
 import '../assets/CSS/layout.css';
 import { flowers } from './FlowerDB';
-export default function Product(){
-    
-    return(
-        <div className="grid-item">
 
-            <div class="card">
-                <img  />
-                <div class="card-body">
-                    <h5 class="card-title">Price:</h5>
-                    <div class="quantity-container">
-                        <label for="quantity">Quantity:</label>
-                        <input type="number" id="quantity" name="quantity" />
+export default function Product() {
+
+    return (
+        <div className="grid-item">
+            {
+                flowers.map((flower) =>
+                    <div class="card">
+                        <img src={require('../assets/image/' + flower.img)}/>
+                        <div class="card-body">
+                            <h5 class="card-title">{flower.name} Price:{flower.price}</h5>
+                            <div class="quantity-container">
+                                <label for="quantity">Quantity:</label>
+                                <input type="number" id="quantity" name="quantity" />
+                            </div>
+                            <button class="card-button">Add to Cart</button>
+                        </div>
                     </div>
-                    <button class="card-button">Add to Cart</button>
-                </div>
-            </div>
+                )
+            }              
         </div>
     );
 }
